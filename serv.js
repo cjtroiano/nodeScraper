77a4b36a6668889d4c19e995c2f391ed6c4c1b9e
cjtroiano <christroiano.ct@gmail.com>
2016-08-16 12:41:19 -0700

    attempted to use promises; didn't work. Now gonna try one big call back

--- serv.js
+++ serv.js
@@ -5,83 +5,89 @@ var cheerio = require('cheerio');
 var promise = require('promise');
 var app     = express();
 
+var box = ["apple", "orange"];
+
+// make somesort of input for a url 
+
+function thenWrite(){
+	return new promise(function(resolve, reject){
+		app.get('/scrape', function(req, res){
+
+			url = 'https://www.mtbproject.com/directory/8007188/bay-area';
+
+			var i = 1;
 
 
-	var box = ["apple", "orange"];
+			//var pageSource = fs.readFileSync(__dirname + "/index.html", "utf8");
+
+			//var page = cheerio.load(pageSource);
+		    //var bd = page('body');
 
 
-app.get('/scrape', function(req, res){
+			//console.log(pageSource);
 
-	url = 'https://www.mtbproject.com/directory/8007188/bay-area';
+			//var promise = new Promise(
 
-	var i = 1;
+			    request(url, function(error, response, html){
+			        if(!error){
+			            var $ = cheerio.load(html);
+			            
+			            $("td").filter(function(){
+			                var data = $(this);
+			                var the = data.text();
+			                var patt = new RegExp("\n{2,}");
+			                var result = patt.exec(the);
+			                //console.log(i, result);
+			                if(result === null){
+			                	var interum = the.split("\n"|',');
+			                	//var interum = pars.split(",");
+			                	//console.log(interum);
+			                	box.push(interum);
+			                	console.log(interum);
+			                	//var wrk = $("<div></div>").text(interum);
+			                	//bd.append(wrk);
+			                	//bd.html();
+			                	//append("<div>"+interum +"</div>")
+			                	// var div = document.createElement("div");
+			                	// var divTxt = document.createTextNode(interum);
+			                	// div.appendChild(divTxt);
+			                	// document.getElementByTagName('body')[0].appendChild(div);
+			                	// //console.log(i, the);
+			                }
+			            });
+			        }else{
+						console.log(error);
+					}
+					//page.append(bd);
+					//console.log(bd);
+					//console.log(pageSource);
+					//resolve(box);
+					var parse = box.toString();
+					res.send(parse);
+			    });
+			 //    .then(function(){
+			 // 		fs.writeFile("check.html", box.toString(), function(){
+			 // 			console.log("file written");
+			 // 		});
+				// }).catch(console.log("promise caught"));
+			//);
+		    //);
 
+		    
 
-	//var pageSource = fs.readFileSync(__dirname + "/index.html", "utf8");
-
-	//var page = cheerio.load(pageSource);
-    //var bd = page('body');
-
-
-	//console.log(pageSource);
-
-
-
-    request(url, function(error, response, html){
-        if(!error){
-
-
-        	
-
-            var $ = cheerio.load(html);
-
-
-            $("td").filter(function(){
-
-
-                var data = $(this);
-                var the = data.text();
-                var patt = new RegExp("\n{2,}");
-                var result = patt.exec(the);
-                //console.log(i, result);
-                if(result === null){
-                	var interum = the.split("\n"|',');
-
-
-                	//var interum = pars.split(",");
-                	//console.log(interum);
-                	box.push(interum);
-                	console.log(interum);
-                	//var wrk = $("<div></div>").text(interum);
-                	//bd.append(wrk);
-                	//bd.html();
-                	//append("<div>"+interum +"</div>")
-                	// var div = document.createElement("div");
-                	// var divTxt = document.createTextNode(interum);
-                	// div.appendChild(divTxt);
-                	// document.getElementByTagName('body')[0].appendChild(div);
-                	// //console.log(i, the);
-                }
-            });
-        }else{
-			console.log(error);
-		}
-		//page.append(bd);
-		//console.log(bd);
-		//console.log(pageSource);
-		var parse = box.toString();
-		res.send(parse);
-        
-    }).then(function(){
-		fs.writeFile("check.html", box.toString(), function(){
-			console.log("file written");
+		    console.log("out of the request!");
 		});
 	});
-    console.log("out of the request!");
-});
+}
 
 app.listen('8080');
 
 console.log('Magic happens on port 8080');
+
+thenWrite().then(function(){
+	fs.writeFile("check.html", box.toString(), function(){
+		console.log("file written");
+	});
+}).catch(console.log("promise caught"));
 
 exports = module.exports = app;
